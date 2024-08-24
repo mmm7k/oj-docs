ef99f316d253c107fa1e4f26bb5ea58ea83b8ff8
computerkingminsu <ya5656@naver.com>
2024-08-24 17:22:21 +0900

    feat: deploy setting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mmm7k.github.io/oj-docs/',
+  url: 'https://mmm7k.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/oj-docs/',
