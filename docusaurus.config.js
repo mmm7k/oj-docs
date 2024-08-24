9117b1e9f46ae95f357bbc002d4618b450b3b25f
computerkingminsu <ya5656@naver.com>
2024-08-25 01:22:32 +0900

    refactor: footer text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Guide',
                 to: '/docs/intro',
               },
             ],
